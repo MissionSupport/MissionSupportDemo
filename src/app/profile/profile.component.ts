39bb0af8e18ebc950d84fc0d60396bd1d11cc55c
rourke750 <rourke7501@gmail.com>
2018-11-20 12:48:22 -0500

    updated profile
    Update support for changing password and email

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,11 +11,55 @@ import {MessageService} from 'primeng/api';
 export class ProfileComponent implements OnInit {
 
   email: string;
+  password: string;
+  newPassword: string;
   constructor(public router: Router, public authInstance: AngularFireAuth, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.email = this.authInstance.auth.currentUser.email;
   }
 
-  saveClick() {
+  saveClick(name: string) {
+    const user = this.authInstance.auth.currentUser;
+    if (name === 'email') {
+      user.updateEmail(this.email).then(() => {
+        // Let's resend an authentication
+        user.sendEmailVerification().then(() => {
+          // Success, now alert user the email verification was sent
+          console.log('Success email updated.');
+          this.messageService.add({severity: 'error', summary: 'Email Updated', detail:
+              'Email was successfully updated'});
+        }).catch(error => {
+          console.log('Error with sending verification email ', error);
+        });
+      }).catch(error => {
+        if (error['code'] === 'auth/invalid-email') {
+          // invalid email
+          this.messageService.add({severity: 'error', summary: 'Email Error', detail: 'Email is invalid'});
+        } else if (error['code'] === 'auth/email-already-in-use') {
+          // email already in use
+          this.messageService.add({severity: 'error', summary: 'Email Error', detail: 'Email is already in use'});
+        } else if (error['code'] === 'auth/requires-recent-login') {
+          // TODO: come back and have user retype in password in order to update email
+          // user.reauthenticateAndRetrieveDataWithCredential(EmailAuthProvider.credential(user.auth.email, password));
+        }
+      });
+    } else if (name === 'password') {
+      if (this.password === this.newPassword) {
+        user.updatePassword(this.password).then(() => {
+          this.messageService.add({severity: 'error', summary: 'Password Changed', detail:
+              'The password was successfully updated'});
+        }).catch(error => {
+          // Failed to update
+          if (error['code]'] === 'auth/weak-password') {
+            // Weak password
+            this.messageService.add({severity: 'error', summary: 'Weak Password', detail:
+                'The password you used was too weak, please try a different one'});
+          } else if (error['code'] === 'auth/requires-recent-login') {
+            // TODO: come back and have user reauthenticate.
+          }
+        });
+      }
+    }
   }
 }
