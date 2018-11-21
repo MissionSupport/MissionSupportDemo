d1c3b5ec9a1bbc88459df1e95a2db320a322da37
Rourke750 <Rourke7501@gmail.com>
2018-11-21 17:24:01 -0500

    seperate services folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {LandingComponent} from './landing/landing.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import {AuthGuard} from './authguard.service';
+import {AuthguardService} from './service/authguard.service';
 import {ProfileComponent} from './profile/profile.component';
 import {SitesComponent} from './sites/sites.component';
 
@@ -18,17 +18,17 @@ const routes: Routes = [
   },
   {
     path: 'landing',
-    canActivate: [AuthGuard],
+    canActivate: [AuthguardService],
     component: LandingComponent
   },
   {
     path: 'profile',
-    canActivate: [AuthGuard],
+    canActivate: [AuthguardService],
     component: ProfileComponent
   },
   {
     path: 'sites/:id',
-    canActivate: [AuthGuard],
+    canActivate: [AuthguardService],
     component: SitesComponent
   }
 ];
