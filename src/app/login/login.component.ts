0685a7e747d5f5582568096fd268e33a77beada0
rourke750 <rourke7501@gmail.com>
2019-01-07 15:19:30 -0500

    Sites (#11)
    * break down editting into methods
    
    split most of the editting features into methods.
    
    * Minor update for tasks
    
    * fix email authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,9 +22,19 @@ export class LoginComponent implements OnInit {
     this.authInstance.auth.setPersistence(auth.Auth.Persistence.SESSION).then(() => {
       console.log('Auth successfully set!');
     });
-    this.authInstance.auth.signInWithEmailAndPassword(this.email, this.password).then(reason => {
-      localStorage.setItem('user', this.authInstance.auth.currentUser.uid);
-      this.router.navigate(['landing']);
+    this.authInstance.auth.signInWithEmailAndPassword(this.email, this.password).then(request => {
+      localStorage.setItem('user', request.user.uid);
+      // Check if user is authenticated
+      if (request.user.emailVerified) {
+        this.router.navigate(['landing']).catch(reason => {
+          console.log('Something went wrong with authguard');
+        });
+      } else {
+        // User was not authenticated
+        this.messageService.add({severity: 'error', summary: 'Login Error', detail: 'Email is not verified'});
+        localStorage.removeItem('user');
+        this.authInstance.auth.signOut();
+      }
     })
       .catch(err => {
         this.messageService.add({severity: 'error', summary: 'Login Error', detail: err});
